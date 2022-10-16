c5d87419f841329d56f6793e341abe7646340705
Ekaterina Chudinova <ek.chudi@ya.ru>
2022-10-16 19:21:36 +0300

    Ex 5 complete

---
+++ ex5_parsing_json.py
@@ -0,0 +1,6 @@
+import json
+
+json_test = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
+obj = json.loads(json_test)
+
+print(obj['messages'][1]['message'])
