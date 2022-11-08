d901364642b0d9a74422a92938f723d5d2b2875b
Ekaterina Chudinova <ek.chudi@ya.ru>
2022-11-08 23:41:59 +0300

    Ex 15 completed

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,30 +1,1 @@
-from lib.my_requests import MyRequests
-from lib.base_case import BaseCase
-from lib.assertions import Assertions
-
-
-class TestUserRegister(BaseCase):
-
-    def test_user_create_successfully(self):
-        data = self.prepare_registration_data()
-
-        response = MyRequests.post("/user", data=data)
-
-        Assertions.assert_code_status(response, 200)
-        Assertions.assert_json_has_key(response, 'id')
-
-    def test_create_user_with_existing_email(self):
-        email = 'vinkotov@example.com'
-        data = self.prepare_registration_data(email)
-
-        response = MyRequests.post("/user", data=data)
-
-        print(response.status_code)
-        print(response.content)
-
-        Assertions.assert_code_status(response, 400)
-        assert response.content.decode('utf-8') == f"Users with email '{email}' already exists", f"Unexpected response content :{response.content}"
-
-# как запустить:
-# python -m pytest -s tests/test_user_register.py
-# python -m pytest -s tests/test_user_register.py -k test_user_create_successfully
+import pytestfrom lib.assertions import Assertionsfrom lib.base_case import BaseCasefrom lib.my_requests import MyRequestsclass TestUserRegister(BaseCase):    exclude_params = [        'password',        'username',        'firstName',        'lastName',        'email'    ]    def test_user_create_successfully(self):        data = self.prepare_registration_data()        response = MyRequests.post("/user", data=data)        Assertions.assert_code_status(response, 200)        Assertions.assert_json_has_key(response, 'id')    def test_create_user_with_existing_email(self):        email = 'vinkotov@example.com'        data = self.prepare_registration_data(email)        response = MyRequests.post("/user", data=data)        Assertions.assert_code_status(response, 400)        assert response.content.decode(            'utf-8') == f"Users with email '{email}' already exists", f"Unexpected response content :{response.content}"    def test_create_user_with_incorrect_email(self):        email = 'emailexample.com'        data = self.prepare_registration_data(email)        response = MyRequests.post("/user", data=data)        Assertions.assert_code_status(response, 400)        assert response.content.decode(            'utf-8') == "Invalid email format"    @pytest.mark.parametrize('condition', exclude_params)    def test_create_user_without_one_of_required_values(self, condition):        data = self.prepare_registration_data()        del data[condition]        response = MyRequests.post("/user", data=data)        Assertions.assert_code_status(response, 400)        assert response.content.decode(            'utf-8') == f"The following required params are missed: {condition}"    def test_create_user_with_too_short_username(self):        data = self.prepare_registration_data()        data['username'] = 'a'        response = MyRequests.post("/user", data=data)        Assertions.assert_code_status(response, 400)        assert response.content.decode(            'utf-8') == "The value of 'username' field is too short"    def test_create_user_with_too_long_username(self):        data = self.prepare_registration_data()        data['username'] = 'a' * 250        response = MyRequests.post("/user", data=data)        Assertions.assert_code_status(response, 400)        assert response.content.decode(            'utf-8') == "The value of 'username' field is too long"# как запустить:# python -m pytest -s tests/test_user_register.py# python -m pytest -s tests/test_user_register.py -k test_user_create_successfully
